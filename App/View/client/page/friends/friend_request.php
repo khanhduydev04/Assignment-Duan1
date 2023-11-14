7a383a5fae4369252fe7fbcf663ec784053d93df
toannt23 <149867771+toannt23@users.noreply.github.com>
2023-11-14 15:10:40 +0700

    [HTML] - Trang Bạn bè (N7 - 13)

---
+++ App/View/client/page/friends/friend_request.php
@@ -0,0 +1,157 @@
+<!-- main -->
+<div class="container-fluid">
+    <div class="row justify-content-evenly">
+        <!-- sidebar -->
+        <div class="col-12 col-lg-3">
+            <div class="d-none d-xxl-block h-100 fixed-top overflow-hidden scrollbar"
+                style="max-width: 390px; width: 100%; z-index: 4; padding-top: 57px;">
+                <div class="shadow p-3 me-5 bg-main">
+                    <ul class="navbar-nav mt-1 ms-1 d-flex flex-column pb-5 mb-2" style="padding-top: 1px">
+                        <div class="
+                         d-flex
+                         justify-content-between
+                         align-items-center
+                         w-100
+                         ">
+                            <p class="m-0 ms-2 fw-bold" style="font-size: 25px;">Bạn bè</p>
+                        </div>
+                        <li class="btb-bg dropdown-item p-1 mt-3 rounded" id="myButton">
+                            <div class="d-flex">
+                                <i class="fa-solid fa-user-group bg-gay p-2 rounded-circle"
+                                    style="font-size: 20px; color: #000;"></i>
+                                <a href="index.html" class="d-flex ms-3
+                             align-items-center
+                             text-decoration-none text-dark">
+                                    <p class="m-0 p-2" style="font-weight: 500; font-size: 17px;">Trang chủ</p>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="btb-bg dropdown-item p-1 mt-3 rounded" id="myButton">
+                            <div class="d-flex">
+                                <i class="fa-solid fa-user-plus bg-gay p-2 rounded-circle" style="font-size: 20px;"></i>
+                                <a href="friend.html" class="d-flex ms-3
+                             align-items-center
+                             text-decoration-none text-dark">
+                                    <p class="m-0 p-2" style="font-weight: 500; font-size: 17px;">Lời mời kết bạn
+                                    </p>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="btb-bg dropdown-item p-1 mt-3 rounded" id="myButton">
+                            <div class="d-flex">
+                                <i class="fa-solid fa-user-group bg-gay p-2 rounded-circle"
+                                    style="font-size: 20px;"></i>
+                                <a href="all-friend.html" class="d-flex ms-3
+                             align-items-center
+                             text-decoration-none text-dark">
+                                    <p class="m-0 p-2" style="font-weight: 500; font-size: 17px;">Tất cả bạn bè</p>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- main -->
+        <div class="col-12 col-lg-9">
+            <div class="d-flex flex-column justify-content-center w-100 mx-auto"
+                style="padding-top: 57px; max-width: 1000px">
+                <div class="mt-3 d-flex justify-content-between position-relative">
+                    <div class="timeline d-flex pb-3 mt-4 justify-content-between align-content-center">
+                        <p class="m-0 fw-bold text-p">Lời mời kết bạn</p>
+                        <p class="m-0 text-blue">Xem tất cả</p>
+                    </div>
+                </div>
+                <div class="d-flex pb-2 mt-0 justify-content-between align-content-center">
+                    <p class="m-0" style="font-weight: 500; color: rgb(65, 113, 194);">5 lời mời kết bạn</p>
+                </div>
+                <div class="pb-3 mt-3 align-content-center justify-content-between">
+                    <div class="row align-items-center">
+                        <div class="col-3">
+                            <img src="./img/av2.jpg" alt="" class="img" style="width: 100%;">
+                            <div class="shadow fb-1" style="padding-top: 1px;">
+                                <div class="d-flex align-content-center justify-content-between">
+                                    <p class="m-0 mt-2 ms-2" style="font-size: 16px; font-weight: 600;">Kiệt Nhỏ</p>
+                                    <p class="m-0 me-3 mt-2 text-secondary" style="font-size: 14px; font-weight: 500;">6
+                                        ngày</p>
+                                </div>
+                                <div class="d-grid gap-2 mx-auto mt-2">
+                                    <input type="button" class="btn btn-primary" value="Xác nhận"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                    <input type="button" class="btn btn-delete" value="Xoá"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <img src="./img/av2.jpg" alt="" class="img" style="width: 100%;">
+                            <div class="shadow fb-1" style="padding-top: 1px;">
+                                <div class="d-flex align-content-center justify-content-between">
+                                    <p class="m-0 mt-2 ms-2" style="font-size: 16px; font-weight: 600;">Kiệt Nhỏ</p>
+                                    <p class="m-0 me-3 mt-2 text-secondary" style="font-size: 14px; font-weight: 500;">2
+                                        ngày</p>
+                                </div>
+                                <div class="d-grid gap-2 mx-auto mt-2">
+                                    <input type="button" class="btn btn-primary" value="Xác nhận"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                    <input type="button" class="btn btn-delete" value="Xoá"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <img src="./img/av2.jpg" alt="" class="img" style="width: 100%;">
+                            <div class="shadow fb-1" style="padding-top: 1px;">
+                                <div class="d-flex align-content-center justify-content-between">
+                                    <p class="m-0 mt-2 ms-2" style="font-size: 16px; font-weight: 600;">Kiệt Nhỏ</p>
+                                    <p class="m-0 me-3 mt-2 text-secondary" style="font-size: 14px; font-weight: 500;">2
+                                        ngày</p>
+                                </div>
+                                <div class="d-grid gap-2 mx-auto mt-2">
+                                    <input type="button" class="btn btn-primary" value="Xác nhận"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                    <input type="button" class="btn btn-delete" value="Xoá"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <img src="./img/av2.jpg" alt="" class="img" style="width: 100%;">
+                            <div class="shadow fb-1" style="padding-top: 1px;">
+                                <div class="d-flex align-content-center justify-content-between">
+                                    <p class="m-0 mt-2 ms-2" style="font-size: 16px; font-weight: 600;">Kiệt Nhỏ</p>
+                                    <p class="m-0 me-3 mt-2 text-secondary" style="font-size: 14px; font-weight: 500;">7
+                                        ngày</p>
+                                </div>
+                                <div class="d-grid gap-2 mx-auto mt-2">
+                                    <input type="button" class="btn btn-primary" value="Xác nhận"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                    <input type="button" class="btn btn-delete" value="Xoá"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center" style="padding-top: 25px;">
+                        <div class="col-3">
+                            <img src="./img/av2.jpg" alt="" class="img" style="width: 100%;">
+                            <div class="shadow fb-1" style="padding-top: 1px;">
+                                <div class="d-flex align-content-center justify-content-between">
+                                    <p class="m-0 mt-2 ms-2" style="font-size: 16px; font-weight: 600;">Kiệt Nhỏ</p>
+                                    <p class="m-0 me-3 mt-2 text-secondary" style="font-size: 14px; font-weight: 500;">6
+                                        ngày</p>
+                                </div>
+                                <div class="d-grid gap-2 mx-auto mt-2">
+                                    <input type="button" class="btn btn-primary" value="Xác nhận"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                    <input type="button" class="btn btn-delete" value="Xoá"
+                                        style="width: 90%; margin-left: 11px; font-weight: 500;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
