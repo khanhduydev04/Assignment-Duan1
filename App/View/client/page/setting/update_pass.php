f04d49aeabf1241e85b37b7c03d307b0ead84a7b
toannt23 <149867771+toannt23@users.noreply.github.com>
2023-11-14 14:59:25 +0700

    [HTML] - Giao diện Cập nhât thông tin/mật khẩu (N7 - 24)

---
+++ App/View/client/page/setting/update_pass.php
@@ -0,0 +1,106 @@
+<!-- main -->
+<div class="container-fluid">
+    <div class="row justify-content-evenly">
+        <!-- sidebar -->
+        <div class="col-12 col-lg-3">
+            <div class="d-none d-xxl-block h-100 fixed-top overflow-hidden scrollbar"
+                style="max-width: 390px; width: 100%; z-index: 4; padding-top: 57px;">
+                <div class="shadow p-3 me-5 bg-main">
+                    <ul class="navbar-nav mt-1 ms-1 d-flex flex-column pb-5 mb-2" style="padding-top: 40px">
+                        <div class="
+                         d-flex
+                         justify-content-between
+                         align-items-center
+                         w-100
+                         ">
+                            <img src="./img/av2.jpg" class="rounded-circle" width="17%" alt="">
+                            <div class="justify-content-center align-content-center pe-4" style="margin-right: 50px;">
+                                <p class="m-0" style="font-size: 17px; font-weight: bold;">Nguyễn Thái Toàn</p>
+                                <p class="m-0">Người dùng facebook</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <li class="dropdown-item p-1 mt-1 rounded">
+                            <div class="d-flex">
+                                <i class="fa-solid fa-house p-2 rounded-circle"
+                                    style="font-size: 20px; color: #000;"></i>
+                                <a href="./uniformation.html" class="d-flex ms-3
+                             align-items-center
+                             text-decoration-none text-dark">
+                                    <p class="m-0 p-2" style="font-weight: 500; font-size: 17px;">Thông tin tài
+                                        khoản</p>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="btn-bg dropdown-item p-1 mt-1 rounded" id="myButton">
+                            <div class="d-flex">
+                                <i class="fa-solid fa-user p-2 rounded-circle"
+                                    style="font-size: 20px;color: #000000;"></i>
+                                <a href="./upd_uinformation.html" class="d-flex ms-3
+                             align-items-center
+                             text-decoration-none text-dark">
+                                    <p class="m-0 p-2" style="font-weight: 500; font-size: 17px;">Cập nhật tài khoản
+                                    </p>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="btn-bg dropdown-item p-1 mt-1 rounded" id="myButton">
+                            <div class="d-flex">
+                                <i class="fa-solid fa-lock p-2 rounded-circle"
+                                    style="font-size: 20px;color: #000000;"></i>
+                                <a href="./upd_pass.html" class="d-flex ms-3
+                             align-items-center
+                             text-decoration-none text-dark">
+                                    <p class="m-0 p-2" style="font-weight: 500; font-size: 17px;">Cập nhật mật khẩu
+                                    </p>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- main -->
+        <div class="col-12 col-lg-9">
+            <div class="d-flex flex-column justify-content-center w-100 mx-auto"
+                style="padding-top: 57px; max-width: 1000px">
+                <div class="mt-3 d-flex justify-content-between position-relative">
+                    <div class="timeline d-flex pb-3 mt-4 justify-content-between align-content-center">
+                        <p class="m-0 fw-bold" style="font-size: 18px;">CẬP NHẬT MẬT KHẨU</p>
+                    </div>
+                </div>
+                <div class="justify-content-between align-content-center">
+                    <form action="" class="mt-3">
+                        <div class="row">
+                            <div class="col-12 col-lg-5">
+                                <label class="form-label" style="font-size: 16px;font-weight: 500;">Mật khẩu hiện
+                                    tại</label>
+                                <input type="password" name="" id="" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-12 col-lg-5">
+                                <label class="form-label" style="font-size: 16px;font-weight: 500;">Mật khẩu
+                                    mới</label>
+                                <input type="password" name="" id="" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-12 col-lg-5">
+                                <label class="form-label" style="font-size: 16px;font-weight: 500;">Xác nhận lại mật
+                                    khẩu</label>
+                                <input type="password" name="" id="" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-md-4">
+                                <button class="btn btn-dark" style="width: 85%;border-radius: 22px;">Cập
+                                    nhật</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
