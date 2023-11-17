86657a68a156eb8d38932d204333e1980823f346
duyvokhanh2506 <125960322+KhanhDuyfpoly@users.noreply.github.com>
2023-11-17 20:59:47 +0700

    [HTML] - Gộp và liên kết code các giao diện (N7 - 51) + [PHP] - Code function Model Friend + User (N7 - 25) + [HTML] - Chỉnh sửa các giao diện (N7 - 50)

--- App/View/client/page/forget_password.php
+++ App/View/client/page/forget_password.php
@@ -1,69 +1,79 @@
-<main>
-    <!--header-->
-    <header class="p-3 shadow">
-        <div class="container-fluid d-flex">
-            <div class="col">
-                <h1 class="text-primary fw-bold fs-2">
-                    Facebook
-                </h1>
-            </div>
-            <div class="col">
-                <form class="row g-2">
-                    <div class="col-auto">
-                      <label for="staticEmail2" class="visually-hidden">Email</label>
-                      <input type="text" readonly class="form-control" id="staticEmail2" placeholder="Email hoặc số điện thoại">
-                    </div>
-                    <div class="col-auto">
-                      <label for="inputPassword2" class="visually-hidden">Password</label>
-                      <input type="password" class="form-control" id="inputPassword2" placeholder="Mật khẩu">
-                    </div>
-                    <div class="col-auto">
-                      <button type="submit" class="btn btn-primary mb-2">Đăng nhập</button>
-                    </div>
-                    <a href="./forget.html" class="col-auto">
-                        <button type="submit" class="btn text-decoration-none link-primary">Bạn quên mật khẩu ư?</button>
-                    </a>
-                </form>
-            </div>
+<!--header-->
+<header class="p-3 shadow">
+    <div class="container-fluid d-flex">
+        <div class="col">
+            <h1 class="text-primary fw-bold fs-2">
+                Facebook
+            </h1>
         </div>
-    </header>
-    <div class="container-fluid bg-gray d-flex justify-content-center p-5">
-        <div class="bg-while shadow rounded pt-3 input-group-lg">
+    </div>
+</header>
+<!-- main -->
+<div class="container-fluid bg-gray d-flex justify-content-center p-5">
+    <?php
+    if (isset($_GET['act'])) {
+        $act = $_GET['act'];
+
+        switch ($act) {
+            case "xacnhan": ?>
+                <div class="bg-white shadow rounded pt-3 input-group-lg" style="width: 28rem;">
+                    <strong class="p-lg-3">Nhập mã xác nhận</strong>
+                    <hr>
+                    <form action="" class="p-lg-2">
+                        <p class="">Vui lòng kiểm tra email để nhận mã xác nhận.</p>
+                        <input type="text" name="code" id="" class="form-control p-3" placeholder="Mã xác nhận">
+                        <hr>
+                        <button type="submit" class="btn btn-primary">Kiểm tra</button>
+                    </form>
+                </div>
+            <?php break;
+            case "capnhatmatkhau": ?>
+                <div class="bg-white shadow rounded p-3 pt-3 input-group-lg" style="width: 28rem;">
+                    <strong class="p-lg-2">Đổi mới mật khẩu</strong>
+                    <hr>
+                    <form action="" class="p-lg-2">
+                        <p class="">Vui lòng nhập mật khẩu mới.</p>
+                        <input type="password" name="" id="" class="form-control px-3 py-2 my-2" placeholder="Mật khẩu mới">
+                        <input type="password" name="" id="" class="form-control px-3 py-2 my-2" placeholder="Xác nhận mật khẩu">
+                        <hr>
+                        <button type="submit" class="btn btn-primary">Cập nhật</button>
+                    </form>
+                </div>
+        <?php break;
+        }
+    } else { ?>
+        <div class="bg-white shadow rounded pt-3 input-group-lg" style="width: 28rem;">
             <strong class="p-lg-3">Tìm tài khoản của bạn</strong>
             <hr>
             <form action="" class="p-lg-2">
-                <p class="">Vui lòng nhập email hoặc số di động để tìm kiếm tài khoản của bạn.</p>
-                <input type="text" name="" id="" class="form-control p-3" placeholder="Email hoặc số điện thoại">
+                <p class="">Vui lòng nhập email tài khoản của bạn.</p>
+                <input type="email" name="email" class="form-control p-3" placeholder="Email">
                 <hr>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end p-2">
                     <button type="button" class="btn btn-secondary">Hủy</button>
-                    <a href="./forget_code.html" class="">
-                        <button type="button" class="btn btn-primary ">Gửi mã</button>
-                    </a>
+                    <button type="submit" class="btn btn-primary">Gửi mã</button>
                 </div>
             </form>
         </div>
+    <?php } ?>
+</div>
+<!-- footer-->
+<footer class=" text-muted ">
+    <!--language-->
+    <div class="d-flex justify-content-center ">
+        <p class="mx-2 fs-7 mb-0">VietNamese</p>
+        <p class="mx-2 fs-7 mb-0">Englist (US)</p>
+        <p class="mx-2 fs-7 mb-0">China</p>
     </div>
-
-    <!-- footer-->
-    <footer class=" text-muted ">
-        <!--language-->
-        <div class="d-flex justify-content-center ">
-            <p class="mx-2 fs-7 mb-0">VietNamese</p>
-            <p class="mx-2 fs-7 mb-0">Englist (US)</p>
-            <p class="mx-2 fs-7 mb-0">China</p>
-        </div>
-        <hr>
-        <!--action-->
-        <div class="d-flex flex-column flex-lg-row justify-content-center ">
-            <p class="mx-2 fs-7 mb-0">Dang nhap</p>
-            <p class="mx-2 fs-7 mb-0">Dang ky</p>
-            <p class="mx-2 fs-7 mb-0">Tin nhan</p>
-        </div>
-        <!--copy-->
-        <div class="mt-4 mx-2 text-center">
-            <p class="fs-7">Facebook & HoDuMaiTran</p>
-        </div>
-
+    <hr>
+    <!--action-->
+    <div class="d-flex flex-column flex-lg-row justify-content-center ">
+        <p class="mx-2 fs-7 mb-0">Dang nhap</p>
+        <p class="mx-2 fs-7 mb-0">Dang ky</p>
+        <p class="mx-2 fs-7 mb-0">Tin nhan</p>
+    </div>
+    <!--copy-->
+    <div class="mt-4 mx-2 text-center">
+        <p class="fs-7">Facebook & HoDuMaiTran</p>
+    </div>
 </footer>
-</main>
--- App/View/client/page/profile.php
+++ App/View/client/page/profile.php
@@ -1,8 +1,8 @@
 <main>
   <div class="pt-5 bg-white w-100 d-flex justify-content-center shadow-sm">
     <div class="profile d-flex flex-column align-items-center">
-      <div class="profile-cover d-flex justify-content-center position-relative">
-        <img src="banner-2.png" alt="" id="image-cover" class="w-100 rounded-3">
+      <div class="profile-cover d-flex justify-content-center position-relative w-100">
+        <img src="./Public/images/banner-men.png" alt="" id="image-cover" class="w-100 rounded-3">
         <!-- Nút mở modal -->
         <button type="button" class="btn mt-2 position-absolute" data-bs-toggle="modal" data-bs-target="#changeCoverModal" style="background-color: rgba(0, 0, 0, 0.4); bottom: 20px; right: 20px;">
           <i class="fa-solid fa-camera" style="color: #ffffff;"></i>
@@ -45,7 +45,7 @@
       </div>
       <div class="main-container d-flex align-items-center position-relative border-bottom px-3 pb-3">
         <div class="profile-avatar position-relative me-4" style="width: 170px; height: 170px; margin-top: -70px;">
-          <img src="avt.png" alt="" id="avatar" class="rounded-circle w-100">
+          <img src="./Public/images/avt.jpg" alt="" id="avatar" class="rounded-circle w-100 h-100" style="object-fit: cover;">
           <!-- Nút mở modal -->
           <button type="button" class="btn position-absolute rounded-circle p-2 d-flex" data-bs-toggle="modal" data-bs-target="#changeAvatarModal" style="background-color: #E4E6EB; bottom: 25px; right: 25px; transform: translate(50%, 50%);">
             <i class="fa-solid fa-camera" style="color: #17191c; font-size: 20px;"></i>
@@ -62,7 +62,6 @@
                   <button type="button" class="btn-close position-absolute" data-bs-dismiss="modal" aria-label="Close" style="right: 20px;"></button>
                 </div>
                 <div class="modal-body">
-
                   <div class="col-12 mb-3">
                     <label for="imageAvatar" id="labelUpload" class="btn btn-outline-primary cursor-pointer w-100 px-1 text-center fw-medium rounded-2">
                       + Tải ảnh lên
@@ -73,8 +72,8 @@
                     <label for="description" class="form-label">Mô tả</label>
                     <textarea name="" id="description" cols="30" rows="10" class="w-100 p-3 form-control" style="max-height: 80px;"></textarea>
                   </div>
-                  <div class="preview col-12 mb-3 mx-auto ">
-                    <img src="" alt="image-preview" class="w-50 rounded-circle" id="imagePreview" style="aspect-ratio: 1/1;">
+                  <div class="preview col-6 mb-3 mx-auto ">
+                    <img src="" alt="image-preview" class="w-100 rounded-circle" id="imagePreview" style="aspect-ratio: 1/1;">
                   </div>
                 </div>
                 <div class="modal-footer" style="display: none;">
@@ -120,9 +119,10 @@
       <div class="main-container w-100%">
         <ul class="m-0 py-2 d-flex align-items-center gap-1 list-unstyled" style="height: 60px;">
           <li class="nav__btn nav__btn-active"><a href="#" class="btn px-4 py-2 text-decoration-none fw-medium">Bài
-              viết</a></li>
-          <li class="nav__btn"><a href="#" class="btn px-4 py-2 text-decoration-none fw-medium">Bạn
-              bè</a></li>
+              viết</a>
+          </li>
+          <li class="nav__btn"><a href="#" class="btn px-4 py-2 text-decoration-none fw-medium">Bạn bè</a>
+          </li>
           <li class="nav__btn"><a href="#" class="btn px-4 py-2 text-decoration-none fw-medium">Ảnh</a>
           </li>
         </ul>
@@ -134,26 +134,26 @@
       <div class="d-none d-lg-flex flex-column gap-3" style="max-width: 426px;">
         <div class="profile-photos bg-white rounded-3 p-3 shadow-sm">
           <div class="pb-3 d-flex justify-content-between align-items-center">
-            <h5><a href="#" class="fs-4 fw-bold text-dark text-decoration-none">Ảnh</a></h5>
-            <a href="#" class="text-decoration-none">Xem tất cả ảnh</a>
+            <h5><a href="index.php?ctrl=profile&act=photos" class="fs-4 fw-bold text-dark text-decoration-none">Ảnh</a></h5>
+            <a href="index.php?ctrl=profile&act=photos" class="text-decoration-none">Xem tất cả ảnh</a>
           </div>
           <div class="rounded-3 overflow-hidden d-flex flex-wrap" style="gap: 4px;">
-            <img src="banner-2.png" alt="" class="profile-img">
-            <img src="banner-2.png" alt="" class="profile-img">
-            <img src="banner-2.png" alt="" class="profile-img">
-            <img src="banner-2.png" alt="" class="profile-img">
-            <img src="banner-2.png" alt="" class="profile-img">
+            <img src="./Public/images/banner-men.png" alt="" class="profile-img">
+            <img src="./Public/images/banner-men.png" alt="" class="profile-img">
+            <img src="./Public/images/banner-men.png" alt="" class="profile-img">
+            <img src="./Public/images/banner-men.png" alt="" class="profile-img">
+            <img src="./Public/images/banner-men.png" alt="" class="profile-img">
           </div>
         </div>
         <div class="profile-friends bg-white rounded-3 p-3 shadow-sm">
           <div class="pb-3 d-flex justify-content-between align-items-center">
-            <h5><a href="#" class="fs-4 fw-bold text-dark text-decoration-none">Bạn bè</a></h5>
-            <a href="#" class="text-decoration-none">Xem tất cả bạn bè</a>
+            <h5><a href="index.php?ctrl=profile&act=friends" class="fs-4 fw-bold text-dark text-decoration-none">Bạn bè</a></h5>
+            <a href="index.php?ctrl=profile&act=friends" class="text-decoration-none">Xem tất cả bạn bè</a>
           </div>
           <div class="d-flex flex-wrap" style="column-gap: 8px;">
             <div class="friend-item mb-2">
               <a href="#">
-                <img src="./banner-2.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
+                <img src="./Public/images/banner-men.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
               </a>
               <a href="#" class="mt-1 d-flex flex-column text-decoration-none text-dark fw-semibold">
                 <span>Khánh Duy</span>
@@ -161,7 +161,7 @@
             </div>
             <div class="friend-item mb-2">
               <a href="#">
-                <img src="./banner-2.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
+                <img src="./Public/images/banner-men.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
               </a>
               <a href="#" class="mt-1 d-flex flex-column text-decoration-none text-dark fw-semibold">
                 <span>Khánh Duy</span>
@@ -169,7 +169,7 @@
             </div>
             <div class="friend-item mb-2">
               <a href="#">
-                <img src="./banner-2.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
+                <img src="./Public/images/banner-men.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
               </a>
               <a href="#" class="mt-1 d-flex flex-column text-decoration-none text-dark fw-semibold">
                 <span>Khánh Duy</span>
@@ -177,7 +177,7 @@
             </div>
             <div class="friend-item mb-2">
               <a href="#">
-                <img src="./banner-2.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
+                <img src="./Public/images/banner-men.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
               </a>
               <a href="#" class="mt-1 d-flex flex-column text-decoration-none text-dark fw-semibold">
                 <span>Khánh Duy</span>
@@ -185,7 +185,7 @@
             </div>
             <div class="friend-item mb-2">
               <a href="#">
-                <img src="./banner-2.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
+                <img src="./Public/images/banner-men.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
               </a>
               <a href="#" class="mt-1 d-flex flex-column text-decoration-none text-dark fw-semibold">
                 <span>Khánh Duy</span>
@@ -193,7 +193,7 @@
             </div>
             <div class="friend-item mb-2">
               <a href="#">
-                <img src="./banner-2.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
+                <img src="./Public/images/banner-men.png" alt="" class="friend-image w-100 object-fit-cover rounded-2">
               </a>
               <a href="#" class="mt-1 d-flex flex-column text-decoration-none text-dark fw-semibold">
                 <span>Khánh Duy</span>
